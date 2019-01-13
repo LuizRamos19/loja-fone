32e99dd4e1d4de950ff58059c1b2c3f434843df2
Luiz Ramos <luizgdramos@gmail.com>
2019-01-13 04:03:18 +0000

    Desafio intermediário aula 014

--- js/product-description.js
+++ js/product-description.js
@@ -5,9 +5,18 @@
 // 5 - No momento que o user clicar, mostrar um ae na tela
 
 const $heart = window.document.querySelector('.-heart');
+const $star = document.querySelector('.-star').querySelectorAll('.icon');
 
 $heart.addEventListener('click', handleClick);
+for (let i = 0; i < $star.length; i++) {
+    $star[i].addEventListener('click', handleStarClick);
+}
 
 function handleClick() {
     $heart.classList.toggle('-active');
+}
+
+
+function handleStarClick() {
+    this.src = 'img/empty-star.png';
 }
