e85c4a44b4f957e67c27ccb791b959e6f262c675
Luiz Ramos <luizgdramos@gmail.com>
2019-01-19 11:31:49 +0000

    Corrigindo desafio avançado da aula 017

--- js/input-search.js
+++ js/input-search.js
@@ -1,7 +1,17 @@
 const $inputSearch = document.querySelector('.input-search');
-const $icon = $inputSearch.querySelector('.icon');
+const $open = $inputSearch.querySelector('.-open');
+const $close = $inputSearch.querySelector('.-close');
 
-$icon.addEventListener('click', event =>  {
+$open.addEventListener('click', event =>  {
     event.preventDefault();
     $inputSearch.classList.toggle('-active');
+    $open.classList.remove('-active');
+    $close.classList.add('-active');
+});
+
+$close.addEventListener('click', event =>  {
+    event.preventDefault();
+    $inputSearch.classList.toggle('-active');
+    $close.classList.remove('-active');
+    $open.classList.add('-active');
 });
